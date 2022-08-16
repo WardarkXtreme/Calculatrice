27c692ca81cc22b4f7c870891c8d2522a9749230
WardarkXtreme <biard.f.pro@gmail.com>
2022-08-16 21:03:11 +0200

    1.0.0.0

---
+++ calculator.js
@@ -0,0 +1,37 @@
+const lineOne = document.querySelector('.lineOne');
+const reset = 0;
+let display = "";
+let operator = null;
+let memory;
+
+window.onload= () => {
+    let keys = document.querySelectorAll(".Btn")
+    for(let key of keys){
+        key.addEventListener("click", managementKeys);
+    }
+}
+function managementKeys(){
+    let key = this.innerText;
+    
+    if(parseFloat(key) >= 0 || key === '.'){
+        display = (display === 0) ? key.toString() : display + key.toString();
+        lineOne.innerText = display;
+        console.log(key);
+    }else{
+        switch(key){
+            case "AC":
+                display= reset;
+                lineOne.innerText = display;
+                operator = null;
+                break;
+            case "C": 
+                let createArray = display.split('')
+                let newValue = createArray.filter((element, index) => index < createArray.length - 1).join('');
+                display = newValue.toString();
+                lineOne.innerText = display;             
+                break;
+            case "M+":
+                break;
+        }
+    }
+}
